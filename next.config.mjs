b360b3440d3734c7423b68c8f1195563c0aaa9e0
Ezra Haugabrooks <ezra@aymscores.com>
2025-08-05 01:39:41 -0400

    fixes to build error related to previous error

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,13 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    // Suppress the Supabase realtime warning
+    config.ignoreWarnings = [
+      /Critical dependency: the request of a dependency is an expression/,
+    ];
+    return config;
+  },
 }
 
 export default nextConfig
